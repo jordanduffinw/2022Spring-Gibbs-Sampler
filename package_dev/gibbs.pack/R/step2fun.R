1e4108ee00d2922939f28467a136e363f12f55dd
a-velazquez <a.m.velazquez@wustl.edu>
2022-05-02 12:45:38 -0500

    finalize step 2 documentation

--- package_dev/gibbs.pack/R/step2fun.R
+++ package_dev/gibbs.pack/R/step2fun.R
@@ -1,24 +1,28 @@
-#' Sample \eqn{\tilde{\beta_j}} in the second step of the MCMC sampler.
+#' Sample \eqn{\tilde{\beta}_j} in the second step of the MCMC sampler.
 #'
 #' For internal use only. Runs the second step of the Gibbs sampler, which samples the beta distribution
 #' created through the defined m and v once, given the input values
 #'
-#' @param X An Nx2 matrix with each column x_i = [\theta_i^{t-1},-1]
-#' @param w_j An Nx1 matrix defined as \eqn{\omega_{ij}^{(t)}}_{i=1}^N
-#' @param y_j A vector of length N containing the count of respondents in group \eqn{i \in 1...N} who
-#' responded to item j affirmatively.
-#' @param n_j A scalar or a vector of length N containing the number of respondents in each unique demographic
+#' @param X An Nx2 matrix with each row \eqn{x_i = [\theta_i^{t-1},-1]}.
+#' @param w_j A vector of length N, \eqn{\omega_{ij}^{(t)}_{i=1}^N}, corresponding to
+#' item j from the matrix output in Step 1.
+#' @param y_j A vector of length N containing the count of respondents in each unique demographic
+#' profile who responded to item j affirmatively.
+#' @param n_j A a vector of length N containing the total number of respondents in each unique demographic
 #' profile who answered item j.
-#' @param Lambda A 2x2 matrix defined as a covariate matrix with diag(0.1)
+#' @param Lambda A scalar, which will be converted into a 2x2 matrix with \code{diag(Lambda)}.
+#' The default is set at 0.1.
 #'
-#' @return A 2x1 matrix \tilde{\beta_j}:
-#'  \item {\tilde{\beta_j}}{The sampled \beta_j from the distribution calculated from the above information}
+#' @return  \eqn{\tilde{\beta}_j}: A vector of length 2 corresponding to
+#' \eqn{[\beta_j, \alpha_j]^T} for response item j.
+#'  \item{beta_tilde_j}{The sampled \eqn{\tilde{\beta}_j} from the multivariate normal distribution}
+#'
 #' @author Jacob Montgomery, Bryant Moy, Noa Dasanaike, Santiago Olivella
 #' @note Currently still need to verify what exactly lambda is in this, add checks, and run a test
 #' @examples
 #'
 #'
-#' @seealso
+#' @seealso step3fun
 #' @aliases step2
 #' @rdname step2fun
 #' @import
@@ -37,7 +41,7 @@ step2fun <- function(X, w_j, y_j, n_j, Lambda=0.1){
   # Turn 1xN jth col vector into diagonal NxN matrix
   Omega_j <- diag(w_j, nrow = N, ncol = N)
 
-  Lambda <- diag(0.1, nrow = 2, ncol = 2)
+  Lambda <- diag(Lambda, nrow = 2, ncol = 2)
 
   # Applying the definition of V_beta
   V_beta <- solve(Lambda + (t(X) %*% Omega_j %*% X))
