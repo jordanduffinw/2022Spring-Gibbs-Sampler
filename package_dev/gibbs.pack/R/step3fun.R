0cbde70c76a025a657e6b8e51bdf4d231c2a9797
a-velazquez <a.m.velazquez@wustl.edu>
2022-05-03 14:08:46 -0500

    update step 3 documentation

--- package_dev/gibbs.pack/R/step3fun.R
+++ package_dev/gibbs.pack/R/step3fun.R
@@ -1,7 +1,7 @@
-#' step3fun
+#' Sample \eqn{\theta_i} in the third step of the MCMC sampler.
 #'
-#' Runs the third step of the Gibbs sampler, which samples \theta ideal point distribution given the function's inputs.
-#'  The returned value is a number.
+#' For internal use only. Runs the third step of the Gibbs sampler, which samples \theta_i ideal point
+#' for a unique demographic profile in 1...N.
 #'
 #' @param sigma2_theta A scalar, default is set to 1.
 #' @param beta_j A Jx1 matrix, or data.frame corresponding to the first column of
@@ -10,9 +10,9 @@
 #' @param y_tilde A single row vector of length J, from the matrix given by
 #'  \eqn{[\{\kappa_{ij} / \omega_{ij}^{(t)} + \alpha_{j}^{(t)}\}_{j=1}^{J}]^T}.
 #'
-#' @return A vector of length 2, containing \eqn{\theta_i^{(t)}} for a unique demographic profile:
+#' @return A single scalar, corresponding to \eqn{\theta_i^{(t)}} for a unique demographic profile:
 #'  \item{\code{theta_i}}{The sampled \eqn{\theta_i^{(t)}} from the from the
-#'  multivariate normal distribution.}
+#'  univariate normal distribution.}
 #'
 #' @author Jacob Montgomery, Bryant Moy, Noa Dasanaike, Santiago Olivella
 #' @note Need some clarity on the other values
@@ -28,7 +28,6 @@
 
 step3fun <- function(beta_j, f_prior_i, y_tilde_i, sigma2_theta=1){
 
-
   # Applying the definition of V_theta
   #betas are current time, t, betas
   V_theta <- as.numeric(solve((1/sigma2_theta) + (t(as.matrix(beta_j)) %*% as.matrix(beta_j))))
@@ -38,8 +37,7 @@ step3fun <- function(beta_j, f_prior_i, y_tilde_i, sigma2_theta=1){
   #f_prior is the prior time's f, f^(t-1)
   m_theta <- as.numeric(V_theta %*% ((f_prior_i / sigma2_theta) + (t(as.matrix(beta_j)) %*% matrix(y_tilde_i, byrow=FALSE))))
 
-  # Attempt at sampling theta from multivariate normal distribution
-  #Note the Sigma here is not the same as the sigma argument
+  # Sample theta from univariate normal distribution
   theta_i <- rnorm(1, mean = m_theta, sd = sqrt(V_theta))
 
   names(theta_i) <- NULL
